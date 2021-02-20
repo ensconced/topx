6eeb5d1b9eabfb601e3550a5407e34984c68e188
Joe Barnett <joebarnett876@gmail.com>
2021-02-20 21:42:50 +0000

    extend the example slightly to show benefit of the topological ordering

--- example.js
+++ example.js
@@ -6,13 +6,19 @@ const a = pipeline.node();
 const b = pipeline.node((aState) => {
   return aState + 2;
 });
+const c = pipeline.node((aState, bState) => {
+  console.log("c state only gets updated once!");
+  return (aState + bState) * 2;
+});
 
 const dependencies = new Map();
 dependencies.set(a, []);
 dependencies.set(b, [a]);
+dependencies.set(c, [a, b]);
 
 pipeline.setDependencies(dependencies);
 
 a.commit(1);
 console.log(a.state);
 console.log(b.state);
+console.log(c.state);
