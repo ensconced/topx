c1a3c9719f6102fa1d596fade2bc9ee52b2103e5
Joe Barnett <joebarnett876@gmail.com>
2021-02-22 21:14:14 +0000

    fix types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,22 +1,16 @@
-declare function makePipeline(): topx.Pipeline;
-export = makePipeline;
-
-declare namespace topx {
-  export type PipelineNode<State> = {
-    name: string;
-    state: State;
-    commit: (newState: State) => void;
-    update: () => void;
-  };
-  export type PipelineGraph = Map<
-    PipelineNode<unknown>,
-    PipelineNode<unknown>[]
-  >;
-  export interface Pipeline {
-    node<State, UpstreamState extends unknown[]>(
-      name: string,
-      updater?: (...upstreamState: UpstreamState) => void
-    ): PipelineNode<State>;
-    setDependencies(dependencyGraph: PipelineGraph): void;
-  }
+declare function makePipeline(): Pipeline;
+export default makePipeline;
+export type PipelineNode<State> = {
+  name: string;
+  state: State;
+  commit: (newState: State) => void;
+  update: () => void;
+};
+export type PipelineGraph = Map<PipelineNode<unknown>, PipelineNode<unknown>[]>;
+export interface Pipeline {
+  node<State, UpstreamState extends unknown[]>(
+    name: string,
+    updater?: (...upstreamState: UpstreamState) => void
+  ): PipelineNode<State>;
+  setDependencies(dependencyGraph: PipelineGraph): void;
 }
